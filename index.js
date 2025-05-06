f9a44f9fab39002361cc1fd6c9af929abfe87bb0
Unprankable01 <121216006+PrestonCurtis1@users.noreply.github.com>
2025-05-06 14:00:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ try {
                 interaction.reply({content: response, fetchReply: true})
             }
             if (interaction.commandName === "giveAura".toLowerCase()){
-                admins = ["770048162395586611","799101657647415337","1215373521463681147","790709753138905129","1305713838775210015","1248851515901481095"];
+                admins = ["770048162395586611","799101657647415337","1215373521463681147","790709753138905129","1305713838775210015","1307191266525839481","1248851515901481095"];
                 let message;
                 if (admins.includes(interaction.user.id)){
                     const target = interaction.options.getUser("user");
