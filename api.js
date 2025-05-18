931236dd1de21b1c085757cab6ca3164063eea04
houdert6 <houdert6@gmail.com>
2025-05-18 13:30:46 -0400

    added /rizzme api

--- api.js
+++ api.js
@@ -26,9 +26,16 @@ async function runApi() {
         let pickuplines = util.getPickupLines();
         res.send(pickuplines);
     });
+    // random rizzme route
+    api.get("/rizzme", async (req, res) => {
+        await util.msg(`[API] /pickuplines`, "1310772622044168275", "1373122799362641971");
+        let pickuplines = util.getPickupLines();
+        const pickupline = pickuplines[Math.floor(Math.random() * pickuplines.length)];
+        res.send({pickupline});
+    });
     api.get("/help",async (req, res) => {
         await util.msg(`[API] /help`, "1310772622044168275", "1373122799362641971");
-        res.send("/getaura\n/getcoins\n/pickuplines\n/help");
+        res.send("/getaura\n/getcoins\n/pickuplines\n/rizzme\n/help");
     });
     // Listen for requests
     api.listen(port, (e) => {
