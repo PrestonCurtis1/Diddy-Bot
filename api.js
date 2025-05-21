f8e5ac0e701e161af5a6db308de88f7a9968c75f
Unprankable01 <121216006+PrestonCurtis1@users.noreply.github.com>
2025-05-20 18:13:58 -0600

    Update api.js

--- api.js
+++ api.js
@@ -10,31 +10,31 @@ async function runApi() {
 
     // Get Aura route
     api.get("/getaura/:userid", async (req, res) => {
-        await util.msg(`[API] /getaura/${req.params.userid}`, "1310772622044168275", "1373122799362641971")
+        await util.msg(`${req.ip}[API] /getaura/${req.params.userid}`, "1310772622044168275", "1373122799362641971")
         let aura = Math.floor(util.User.getUser(req.params.userid)?.aura ?? 0);
         res.send({aura});
     });
     // Get Coins route
     api.get("/getcoins/:guildid/:userid", async (req, res) => {
-        await util.msg(`[API] /getcoins/${req.params.guildid}/${req.params.userid}`, "1310772622044168275", "1373122799362641971")
+        await util.msg(`${req.ip}[API] /getcoins/${req.params.guildid}/${req.params.userid}`, "1310772622044168275", "1373122799362641971")
         let coins = util.User.getUser(req.params.userid)?.getCoins(util.Guild.getGuild(req.params.guildid)) ?? 0;
         res.send({coins});
     });
     // rizzme messages route
     api.get("/pickuplines", async (req, res) => {
-        await util.msg(`[API] /pickuplines`, "1310772622044168275", "1373122799362641971");
+        await util.msg(`${req.ip}[API] /pickuplines`, "1310772622044168275", "1373122799362641971");
         let pickuplines = util.getPickupLines();
         res.send(pickuplines);
     });
     // random rizzme route
     api.get("/rizzme", async (req, res) => {
-        await util.msg(`[API] /rizzme`, "1310772622044168275", "1373122799362641971");
+        await util.msg(`${req.ip}[API] /rizzme`, "1310772622044168275", "1373122799362641971");
         let pickuplines = util.getPickupLines();
         const pickupline = pickuplines[Math.floor(Math.random() * pickuplines.length)];
         res.send({pickupline});
     });
     api.get("/help",async (req, res) => {
-        await util.msg(`[API] /help`, "1310772622044168275", "1373122799362641971");
+        await util.msg(`${req.ip}[API] /help`, "1310772622044168275", "1373122799362641971");
         res.send("/getaura\n/getcoins\n/pickuplines\n/rizzme\n/help");
     });
     // Listen for requests
